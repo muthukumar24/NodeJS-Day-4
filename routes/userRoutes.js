c933ce2dbff14e0c31f0cbb1223e1b33f0ca7342
Muthu kumar <muthukumar24081999@gmail.com>
2024-05-31 13:38:12 +0530

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// create a new user
+router.post("/users", async (req, res)=>{
+    try {
+        const user = new User(req.body);
+        console.log("Testing DB")
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// get all users
+router.get("/users", async (req, res)=>{
+    try {
+        const users = await User.find({});
+        res.status(201).send(users);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// get a user by ID
+router.get("/users/:id", async (req, res)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user){
+            res.status(400).send();
+        }
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+// Update a user by ID
+router.put("/users/:id", async (req, res)=>{
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body,
+            {new: true, runValidators: true}
+        );
+        if(!user){
+            res.status(404).send();
+        }
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+// Delete user a user by ID
+router.delete("/users/:id", async (req, res)=>{
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            res.status(400).send();
+        }
+        res.status(201).send(user);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const boydParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(boydParser.json())
+// routes
+app.use('/apiUser', userRoutes);
+
+mongoose.connect(process.env.MONGODB)
+.then(()=>{
+    console.log("Connected to MongoDB");
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
+.catch(error => {
+    console.error("Connection Error", error.message);
+});
